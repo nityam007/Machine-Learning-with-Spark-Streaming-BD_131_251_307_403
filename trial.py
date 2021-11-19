b6cf98813f7ccdf1c3cc461c3ecd2220562758dd
Nityam Churamani <68228031+nityam007@users.noreply.github.com>
2021-11-19 10:17:16 +0530

    Reading data incrementally and storing data in a list

--- trial.py
+++ trial.py
@@ -1,20 +1,62 @@
 from pyspark import SparkContext
 from pyspark.streaming import StreamingContext
+import pandas as pd
+import json 
+from pyspark.sql import SparkSession
+import sys
+import re
+import operator
+from pyspark.ml import Pipeline
+import numpy 
+
 
 # Create a local StreamingContext with two working thread and batch interval of 1 second
 sc = SparkContext("local[2]", "NetworkWordCount")
 ssc = StreamingContext(sc, 1)
 
+spark = SparkSession.builder.appName('sparkdf').getOrCreate()
 
 lines = ssc.socketTextStream("localhost", 6100)
 
-#words = lines.flatMap(lambda line: line.split(" "))
+#json_line = json.loads(lines)
+#words = json_lines.flatMap(lambda json_line: json_line.split("\n"))
 
-#pairs = words.map(lambda word: (word, 1))
-#wordCounts = pairs.reduceByKey(lambda x, y: x + y)
+def readmystream(rdd):
+	df = spark.read.json(rdd)
 
-# Print the first ten elements of each RDD generated in this DStream to the console
-lines.pprint() 
+	#df.printschema()
+	
+	#df.show()	
+	
+	f=df.collect()
+	#df.show()
+	 
+	f0=[]
+	f1=[]
+	f2=[]
+	
+	for i in f:
+		for k in i:
+			#for l in k:
+			f0.append(k[0])
+			f1.append(k[1])
+			f2.append(k[2])
+	print(f0)
+	print(len(f0))
+	
+	print(f1)
+	print(len(f1))
+	
+	print(f2)
+	print(len(f2))
+	
+		
+		#print(f"{i}\t{j}\t{k}")
+	
+	
+
+lines.foreachRDD(lambda rdd: readmystream (rdd))
+
 
 ssc.start()
 
