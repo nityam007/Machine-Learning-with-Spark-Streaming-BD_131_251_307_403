210347c953477ea667c1691cecb98ab76f9c780f
Darshil Shah <58663553+darshil-geek@users.noreply.github.com>
2021-11-23 12:11:59 +0530

    added random forest classifier

--- classification/pipeline.py
+++ classification/pipeline.py
@@ -3,7 +3,8 @@ from pyspark.ml import Pipeline
 
 from pyspark.ml.classification import NaiveBayes
 from pyspark.ml.evaluation import BinaryClassificationEvaluator
-
+from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.classification import RandomForestClassifier
 def naiveBayesClassifierGo(train,test):
     
     nb = NaiveBayes(smoothing=1.0, modelType="multinomial")
@@ -15,6 +16,45 @@ def naiveBayesClassifierGo(train,test):
     evaluator = BinaryClassificationEvaluator(rawPredictionCol="prediction")
     accuracy = evaluator.evaluate(predictions)
     print ("Model Accuracy: ", accuracy)
+
+
+
+def randomForestCLassifier(train,test,data):
+
+    labelIndexer = StringIndexer(inputCol="spam/ham", outputCol="indexedLabel").fit(data)
+
+    # Automatically identify categorical features, and index them.
+    # Set maxCategories so features with > 4 distinct values are treated as continuous.
+    featureIndexer =\
+        VectorIndexer(inputCol="features", outputCol="indexedFeatures", maxCategories=4).fit(data)
+
+    # Split the data into training and test sets (30% held out for testing)
+    #(trainingData, testData) = data.randomSplit([0.7, 0.3])
+
+    # Train a RandomForest model.
+    rf = RandomForestClassifier(labelCol="indexedLabel", featuresCol="indexedFeatures", numTrees=10)
+
+    # Convert indexed labels back to original labels.
+    labelConverter = IndexToString(inputCol="prediction", outputCol="predictedLabel",
+                                labels=labelIndexer.labels)
+
+    # Chain indexers and forest in a Pipeline
+    pipeline = Pipeline(stages=[labelIndexer, featureIndexer, rf, labelConverter])
+
+    # Train model.  This also runs the indexers.
+    model = pipeline.fit(train)
+
+    # Make predictions.
+    predictions = model.transform(test)
+
+    # Select example rows to display.
+    predictions.select("spam/ham","predictedLabel").show(5)
+
+    # Select (prediction, true label) and compute test error
+    evaluator = MulticlassClassificationEvaluator(
+        labelCol="indexedLabel", predictionCol="prediction", metricName="accuracy")
+    accuracy = evaluator.evaluate(predictions)
+    print("Test Error = %g" % (1.0 - accuracy))
 
 
 
@@ -43,4 +83,5 @@ def sender(df):
     data = pipeline.fit(df).transform(df)
     train, test = data.randomSplit([0.7, 0.3], seed = 2018)
      
-    naiveBayesClassifierGo(train,test)
+    #naiveBayesClassifierGo(train,test)
+    randomForestCLassifier(train,test,data)
